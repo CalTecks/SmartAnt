00717581c9e630bb0cdef37fb53e27c5e0cb7a68
CalTecks <thomas.van.beeck@pandora.be>
2024-05-05 17:47:57 +0200

    cylinders goud bij Hit, kleinere testopstelling, redelijke testresultaten

--- Assets/ScoreTrigger.cs
+++ Assets/ScoreTrigger.cs
@@ -4,9 +4,12 @@ public class ScoreTrigger : MonoBehaviour
 {
     public AnimalAgent animalAgent;
     private bool isCrushed = false;
+    public Material hitMaterial;
+    private Renderer objectRenderer;
 
     void Start() {
         animalAgent = GameObject.FindWithTag("AnimalAgent").GetComponent<AnimalAgent>();
+        objectRenderer = GetComponent<Renderer>();
     }
 
     private void OnCollisionStay(Collision collision)
@@ -20,6 +23,7 @@ public class ScoreTrigger : MonoBehaviour
                 animalAgent.TargetCrushed();
                 // dit target is geraakt, dus isCrushed = true
                 isCrushed = true;
+                objectRenderer.material = hitMaterial;
             }
         }
     }
